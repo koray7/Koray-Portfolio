b1762abf7cbdd5517db04870b13cdf89175e4f5d
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-05-31 11:09:54 -0500

    time message

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,9 @@ const Navbar = () => {
             <Link className="navbar" to="/about">About</Link>
             <Link className="navbar" to="/projects">Projects</Link>
             <Link className="navbar" to="/contact">Contact</Link>
-            
+            <div>
+            <span className="deepNote">The most important thing in life, use it wisely..</span>
+            </div>
             <div className="timeDiv">
                     <h1 className="time">{time}</h1>
                     <h3 className="day">{today}</h3>
