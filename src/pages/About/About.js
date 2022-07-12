662541df67eb647c8d395174826c35217d808d5a
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-07-12 11:04:12 -0500

    Projects Updated

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,64 +1,107 @@
 import React from "react";
 
 const About = () => {
-    return(
-        <div className="aboutPage">
-            <div>
-                <img src="https://i.postimg.cc/Hxnc7r56/IMG-9528.jpg" className="self-img" alt='Self-Img'/>
-            </div>
-                <div className="about-page-bio">
-                    <span id="about-page-text">Who is "Koray Kurtoglu?"</span><br></br>
-                    <h5 className="about-page-text1">
-                        Born and raised in Turkey. I studied Business and Hotel Management at one of the biggest university in South of Turkey. And then came to United States back in 2015. It was a great experience as a front desk agent and as a supervisor at one of the best hotel in Fort Lauderdale, FL. I would say, It was an amazing feeling that my talents has been seen, all the way from overseas. After a year, contract ended, but my journey just started.
-                    </h5>
-                <div className="education">
-                    <h3 className="about-page-topics">Education</h3>
-                    <h5 className="about-page-education">
-                        GENERAL ASSEMBLY
-                    </h5>
-                    <h6 className="about-page-education">
-                    Software Engineering Immersive | Full Stack Web Developer | AUSTIN, TX | OCTOBER 2021-MARCH 2022
-                    </h6>
-                    <h5 className="about-page-education">
-                        UDEMY AND FREE CODE CAMP ONLINE CLASSES
-                    </h5>
-                    <h6 className="about-page-education">
-                    Full Stack Web Development | SEPTEMBER 2021-MARCH 2022
-                    </h6>
-                </div>
-                <div className="experiences">
-                    <h3 className="about-page-topics">Experiences</h3>
-                    <h5 className="about-page-experience">
-                    🧑🏻‍💻SOFTWARE ENGINEERING IMMERSIVE🧑🏻‍💻<br></br>
-                        General Assembly | Washington, D.C. | December 2021-March 2022
-                        Successfully completed 500+ hours of expert-led applied computer science fundamentals using the industry's most in demand technologies. Learned Front-end and Back-end languages including HTML, CSS, JavaScript, ReactJS, NodeJS, Express, Python and Django and SQL database.<br></br><br></br>
-                        ● Developed projects, including a game called “CARD OF WAR” using HTML, CSS, JavaScript.<br></br>
-                        ● LavenWoods Alchemy (Developed within a group of 4 people)<br></br>
-                        ● Node.js, Express, MangoDB and Mongoose. It’s also a full CRUD project with a RESTful
-                        layout.<br></br>
-                        ● SportForLife-ReactJS, bootstrap, API technology, Express, Heroku for deployment.<br></br>
-                        ● RightAway-Python, Django and SQL database.
-                    </h5>
-                </div>
-                <div className="contact">
-                    <h3 className="about-page-topics">Contact information
-                    </h3>
-                    <a href="https://www.linkedin.com/in/koray-kurtoglu/" target="_blank" rel="noReferrer">
-                        <img src="https://www.logo.wine/a/logo/LinkedIn/LinkedIn-Wordmark-White-Dark-Background-Logo.wine.svg" id="social-logo" alt='LinkedIn' />
-                    </a>
-                    <a href="https://github.com/koray7" target="_blank" rel="noReferrer">
-                        <img src="https://media.xconomy.com/wordpress/wp-content/images/2016/06/06161811/github-logo.jpg" id="social-logo" alt='GitHub' />
-                    </a>
-                    <a href="mailto:softwarekoray@gmail.com" target="_blank" rel="noReferrer">
-                        <img src="https://cdn.vox-cdn.com/thumbor/8fWz6qpiMYMsZhY4vrc9Vhl5yL8=/0x110:1320x770/fit-in/1200x600/cdn.vox-cdn.com/uploads/chorus_asset/file/21939811/newgmaillogo.jpg" id="social-logo" alt='Gmail' />
-                    </a>
-                    <a href="https://docs.google.com/document/d/1tncNRrMLRyGLbMTf7anEV4pxORd_C1YuTkgOaUQqRhg/edit?usp=sharing" download="Koray-Resume.pdf" target="_blank" rel="noReferrer"><img src="https://www.conovercompany.com/wp-content/uploads/2019/06/wr-resumes-logo.png" id="social-logo" alt='Koray-Resume' />
-                    </a>
-                </div>
-            </div>
+  return (
+    <div className="aboutPage">
+      <div>
+        <img
+          src="https://i.postimg.cc/Hxnc7r56/IMG-9528.jpg"
+          className="self-img"
+          alt="Self-Img"
+        />
+      </div>
+      <div className="about-page-bio">
+        <span id="about-page-text">Who is "Koray Kurtoglu?"</span>
+        <br></br>
+        <h5 className="about-page-text1">
+          Born and raised in Turkey. I studied Business and Hotel Management at
+          one of the biggest university in South of Turkey. And then came to
+          United States back in 2015. It was a great experience as a front desk
+          agent and as a supervisor at one of the best hotel in Fort Lauderdale,
+          FL. I would say, It was an amazing feeling that my talents has been
+          seen, all the way from overseas. After a year, contract ended, but my
+          journey just started.
+        </h5>
+        <div className="education">
+          <h3 className="about-page-topics">Education</h3>
+          <h5 className="about-page-education">GENERAL ASSEMBLY</h5>
+          <h6 className="about-page-education">
+            Software Engineering Immersive | Full Stack Web Developer | AUSTIN,
+            TX | OCTOBER 2021-MARCH 2022
+          </h6>
+          <h5 className="about-page-education">
+            UDEMY AND FREE CODE CAMP ONLINE CLASSES
+          </h5>
+          <h6 className="about-page-education">
+            Full Stack Web Development | SEPTEMBER 2021-MARCH 2022
+          </h6>
         </div>
-    )
-}
+        <div className="experiences">
+          <h3 className="about-page-topics">Experiences</h3>
+          <h5 className="about-page-experience">
+            🧑🏻‍💻SOFTWARE ENGINEERING IMMERSIVE🧑🏻‍💻<br></br>
+            General Assembly | Washington, D.C. | December 2021-March 2022
+            Successfully completed 500+ hours of expert-led applied computer
+            science fundamentals using the industry's most in demand
+            technologies. Learned Front-end and Back-end languages including
+            HTML, CSS, JavaScript, ReactJS, NodeJS, Express, Python and Django
+            and SQL database.<br></br>
+            <br></br>● Developed projects, including a game called “CARD OF WAR”
+            using HTML, CSS, JavaScript.<br></br>● LavenWoods Alchemy (Developed
+            within a group of 4 people)<br></br>● Node.js, Express, MangoDB and
+            Mongoose. It’s also a full CRUD project with a RESTful layout.
+            <br></br>● SportForLife-ReactJS, bootstrap, API technology, Express,
+            Heroku for deployment.<br></br>● RightAway-Python, Django and SQL
+            database.
+          </h5>
+        </div>
+        <div className="contact">
+          <h3 className="about-page-topics">Contact information</h3>
+          <a
+            href="https://www.linkedin.com/in/koray-kurtoglu/"
+            target="_blank"
+            rel="noReferrer"
+          >
+            <img
+              src="https://www.logo.wine/a/logo/LinkedIn/LinkedIn-Wordmark-White-Dark-Background-Logo.wine.svg"
+              id="social-logo"
+              alt="LinkedIn"
+            />
+          </a>
+          <a href="https://github.com/koray7" target="_blank" rel="noReferrer">
+            <img
+              src="https://media.xconomy.com/wordpress/wp-content/images/2016/06/06161811/github-logo.jpg"
+              id="social-logo"
+              alt="GitHub"
+            />
+          </a>
+          <a
+            href="mailto:softwarekoray@gmail.com"
+            target="_blank"
+            rel="noReferrer"
+          >
+            <img
+              src="https://cdn.vox-cdn.com/thumbor/8fWz6qpiMYMsZhY4vrc9Vhl5yL8=/0x110:1320x770/fit-in/1200x600/cdn.vox-cdn.com/uploads/chorus_asset/file/21939811/newgmaillogo.jpg"
+              id="social-logo"
+              alt="Gmail"
+            />
+          </a>
+          <a
+            href="https://docs.google.com/document/d/1tncNRrMLRyGLbMTf7anEV4pxORd_C1YuTkgOaUQqRhg/edit?usp=sharing"
+            download="Koray-Resume.pdf"
+            target="_blank"
+            rel="noReferrer"
+          >
+            <img
+              src="https://www.conovercompany.com/wp-content/uploads/2019/06/wr-resumes-logo.png"
+              id="social-logo"
+              alt="Koray-Resume"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default About;
-
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,39 +1,87 @@
 import React, { useState } from "react";
 
-
 const Home = () => {
-
-    return(
-        <div className="homePage">
-            <div className="homePage1">
-                <div className="self-img-div">
-                <img src="https://iili.io/Gveul2.md.jpg" className="img-self" alt='SelfImg' />
-                </div>
-                
-                <h1 className="myName">Koray Adams</h1>
-                <h2>Full Stack Web Developer🧑🏻‍💻</h2>
-                <div className="main-page-logo">
-                    <img src="https://www.html5rocks.com/static/demos/svgmobile_fundamentals/images/html5-640x960.jpg" id="social-logo" alt='Html' />
-                    <img src="https://encoresky.com/blog/wp-content/uploads/2020/03/1561458_7f3b.jpg" id="social-logo" alt='CSS' />
-                    <img src="https://www.pngitem.com/pimgs/m/720-7208829_javascript-logo-png-transparent-png.png" id="social-logo" alt='JavaScript' />
-                    <img src="https://quantlabs.net/wp-content/uploads/2019/12/python-main-qimg-28cadbd02699c25a88e5c78d73c7babc.png" id="social-logo" alt='Python' />
-                    <img src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png" id="social-logo" alt='ReactJS' />
-                    <img src="https://w7.pngwing.com/pngs/1005/334/png-transparent-express-js-node-js-javascript-session-web-application-others.png" id="social-logo" alt='NodeJS' />
-                    
-                    <img src="https://i0.wp.com/useforcode.com/wp-content/uploads/2021/04/225-2254691_9kib-354x415-unnamed-mongodb-logo-svg.jpg?fit=820%2C454&ssl=1" id="social-logo" alt='MongoDB' />
-                    <img src="https://webobjects2.cdw.com/is/image/CDW/5942177?$product-main$" id="social-logo" alt='SQL' />
-                    <img src="https://miro.medium.com/max/640/1*75jvBleoQfAZJc3sgTSPQA.jpeg" id="social-logo" alt='GitHub' />
-                    <img src="https://logodix.com/logo/1758777.jpg" id="social-logo" alt='Django' />
-                </div>
-            </div>
-            <div className="intro">
-                <h1>INTRODUCTION</h1>
-                <h4 id="intro-text">
-                    I'm resilient and disciplined in staying focused on completing my goals until my desired outcome is met, and I love going above and beyond that. I'm also a career driven person and I have a "lifetime learning" mindset. I'm a good team player, hardworking, positive and fully motivated person. Every role that I take in the company, I would love to add international perspectives and useful ideas on top of my role to make the job easier and effective.
-                </h4>
-            </div>
+  return (
+    <div className="homePage">
+      <div className="homePage1">
+        <div className="self-img-div">
+          <img
+            src="https://iili.io/Gveul2.md.jpg"
+            className="img-self"
+            alt="SelfImg"
+          />
         </div>
-    )
-}
+
+        <h1 className="myName">Koray Adams</h1>
+        <h2>Full Stack Web Developer🧑🏻‍💻</h2>
+        <div className="main-page-logo">
+          <img
+            src="https://www.html5rocks.com/static/demos/svgmobile_fundamentals/images/html5-640x960.jpg"
+            id="social-logo"
+            alt="Html"
+          />
+          <img
+            src="https://encoresky.com/blog/wp-content/uploads/2020/03/1561458_7f3b.jpg"
+            id="social-logo"
+            alt="CSS"
+          />
+          <img
+            src="https://www.pngitem.com/pimgs/m/720-7208829_javascript-logo-png-transparent-png.png"
+            id="social-logo"
+            alt="JavaScript"
+          />
+          <img
+            src="https://quantlabs.net/wp-content/uploads/2019/12/python-main-qimg-28cadbd02699c25a88e5c78d73c7babc.png"
+            id="social-logo"
+            alt="Python"
+          />
+          <img
+            src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png"
+            id="social-logo"
+            alt="ReactJS"
+          />
+          <img
+            src="https://w7.pngwing.com/pngs/1005/334/png-transparent-express-js-node-js-javascript-session-web-application-others.png"
+            id="social-logo"
+            alt="NodeJS"
+          />
+
+          <img
+            src="https://i0.wp.com/useforcode.com/wp-content/uploads/2021/04/225-2254691_9kib-354x415-unnamed-mongodb-logo-svg.jpg?fit=820%2C454&ssl=1"
+            id="social-logo"
+            alt="MongoDB"
+          />
+          <img
+            src="https://webobjects2.cdw.com/is/image/CDW/5942177?$product-main$"
+            id="social-logo"
+            alt="SQL"
+          />
+          <img
+            src="https://miro.medium.com/max/640/1*75jvBleoQfAZJc3sgTSPQA.jpeg"
+            id="social-logo"
+            alt="GitHub"
+          />
+          <img
+            src="https://logodix.com/logo/1758777.jpg"
+            id="social-logo"
+            alt="Django"
+          />
+        </div>
+      </div>
+      <div className="intro">
+        <h1>INTRODUCTION</h1>
+        <h4 id="intro-text">
+          I'm resilient and disciplined in staying focused on completing my
+          goals until my desired outcome is met, and I love going above and
+          beyond that. I'm also a career driven person and I have a "lifetime
+          learning" mindset. I'm a good team player, hardworking, positive and
+          fully motivated person. Every role that I take in the company, I would
+          love to add international perspectives and useful ideas on top of my
+          role to make the job easier and effective.
+        </h4>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
